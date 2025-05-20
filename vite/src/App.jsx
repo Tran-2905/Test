0c386e5213dda90098de2cbad1b4456df9e4d683
Royce Tran <78252279+Tran-2905@users.noreply.github.com>
2025-05-20 22:30:43 +0700

    second

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -2,9 +2,12 @@ import './App.css'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <h1>dafbka</h1>
+    </div>
   )
 }
 
